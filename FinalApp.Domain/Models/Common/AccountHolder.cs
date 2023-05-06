347ef09fa350b88ec375abe4b75a0b4a0e5f91fe
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-06 17:37:17 +0600

    Add: Common Entities Added

---
+++ FinalApp.Domain/Models/Common/AccountHolder.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalApp.Domain.Models.Common
+{
+    public abstract class AccountHolder : BaseEntity
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ FinalApp.Domain/Models/Common/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace FinalApp.Domain.Models.Common
+{
+    public abstract class Person : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{Name} {Surname}";
+            }
+        }
+    }
+}
