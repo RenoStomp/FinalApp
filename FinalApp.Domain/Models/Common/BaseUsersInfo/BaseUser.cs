024ba256e7802da9d6486ecafe82fad7a1470db5
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-12 15:58:47 +0600

    edit: Base user edited

--- FinalApp.Domain/Models/Common/BaseUsersInfo/BaseUser.cs
+++ FinalApp.Domain/Models/Common/BaseUsersInfo/BaseUser.cs
@@ -4,7 +4,7 @@ namespace FinalApp.Domain.Models.Common.BaseUsersInfo
 {
     public abstract class BaseUser : AccountHolder
     {
-        public abstract Roles UserType { get; set; }
+        public virtual Roles UserType { get; set; }
         public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
 
     }
