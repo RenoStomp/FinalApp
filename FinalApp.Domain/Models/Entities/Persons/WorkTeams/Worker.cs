4a7da2350d398e2425850faab194b36373d21d5b
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-12 16:17:30 +0600

    Change: Little changes

--- FinalApp.Domain/Models/Entities/Persons/WorkTeams/Worker.cs
+++ FinalApp.Domain/Models/Entities/Persons/WorkTeams/Worker.cs
@@ -6,7 +6,6 @@ namespace FinalApp.Domain.Models.Entities.Persons.WorkTeams
 {
     public class Worker : PersonalContactInfo
     {
-
         public string Salary { get; set; }
         public DateTime HireTime { get; set; }= DateTime.UtcNow;
         public Roles Position { get; set; } = Roles.TechnicalWorker;
