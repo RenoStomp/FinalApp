695e8c122ab9f83a8b2e0433e9288f4ada4c952d
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-12 12:09:35 +0600

    edit: base Location edited 2

--- FinalApp.Domain/Models/Common/BaseRequests/BaseLocation.cs
+++ FinalApp.Domain/Models/Common/BaseRequests/BaseLocation.cs
@@ -9,23 +9,17 @@ namespace FinalApp.Domain.Models.Common.BaseRequests
         public string HouseNumber { get; set; }
         public string? ApartmentNumber { get; set; }
         public string ZipCode { get; set; }
-        public Dictionary<AddressComponent, string> Address
+        public string Address
         {
             get
             {
-                var address = new Dictionary<AddressComponent, string>
-                {
-                    { AddressComponent.City, City },
-                    { AddressComponent.Street, Street },
-                    { AddressComponent.HouseNumber, HouseNumber },
-                    { AddressComponent.ZipCode, ZipCode }
-                };
-                if (ApartmentNumber != null)
-                {
-                    address.Add(AddressComponent.ApartmentNumber, ApartmentNumber);
-                }
-
-                return address;
+                return (
+                    $"City: {City}\n" +
+                    $"Street: {Street}\n" +
+                    $"HouseNumber: {HouseNumber}\n" +
+                    $"ApartmentNumber: {ApartmentNumber}\n" +
+                    $"ZipCode {ZipCode}"
+                    );
             }
         }
     }
