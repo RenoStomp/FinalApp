fdfad55cfc115507fb939957154bc8ca34939514
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-13 16:30:22 +0600

    add: AppdbContext and all tables added

---
+++ FinalApp.DAL/SqlServer/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using FinalApp.Domain.Models.Entities.Persons.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalApp.DAL.SqlServer
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Client>
+    }
+}
