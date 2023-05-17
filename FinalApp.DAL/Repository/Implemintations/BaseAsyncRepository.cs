11e04434fcfac886ffad3a6aaf3e26594f140d06
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-17 20:02:45 +0600

    ADD: Create Read and Update added to Repository

--- FinalApp.DAL/Repository/Implemintations/BaseAsyncRepository.cs
+++ FinalApp.DAL/Repository/Implemintations/BaseAsyncRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FinalApp.DAL.Repository.Interfaces;
+using FinalApp.DAL.SqlServer;
+using FinalApp.Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,74 @@ using System.Threading.Tasks;
 
 namespace FinalApp.DAL.Repository.Implemintations
 {
-    internal class BaseAsyncRepository
+    public class BaseAsyncRepository<T> : IAsyncReposytory<T> 
+        where T : BaseEntity
     {
+        private readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public BaseAsyncRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+
+            if (_dbSet == default(DbSet<T>))
+                throw new ArgumentNullException(nameof(DbSet<T>));
+        }
+
+        public async Task Create(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        public IQueryable<T> ReadAll()
+        {
+            return _dbSet;
+        }
+        public async Task<IQueryable<T>> ReadAllAsync()
+        {
+            return await Task.FromResult(_dbSet);
+        }
+        public T ReadById(int id)
+        {
+            var entity = ReadAll().FirstOrDefault(x => x.Id == id);
+
+            return entity == null
+             ? throw new ArgumentNullException(nameof(id), $"Entity not found by id {id}")
+             : entity;
+        }
+        public async Task<T> ReadByIdAsync(int id)
+        {
+            var entity = await ReadAllAsync().Result.FirstOrDefaultAsync(x => x.Id == id);
+
+            return entity == null
+            ? throw new ArgumentNullException(nameof(id), $"Entity not found by id {id}")
+            : entity;
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task DeleteAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+   
+
+   
+
+       
+
+ 
+
+
     }
 }
