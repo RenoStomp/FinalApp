7c7b769fc7ec75a3f6f5e2ae21bcac465500b699
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-17 19:29:42 +0600

    Add: IAsyncRepository and summary  added

---
+++ FinalApp.DAL/Repository/Interfaces/IAsyncReposytory.cs
@@ -0,0 +1,27 @@
+﻿using FinalApp.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalApp.DAL.Repository.Interfaces
+{
+    public interface IAsyncReposytory<T>
+        where T : BaseEntity
+    {
+        /// <summary>
+        /// Creates one object and adds it to database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public Task Create(T entity);           // C - Create
+        public IQueryable<T> Get();             // R - Read all
+        public Task<IQueryable<T>> GetAsync();  // R - Read all async
+        public T GetById(int id);               // R - Read one
+        public Task<T> GetByIdAsync(int id);    // R - Read one async
+        public Task Update(T Item);             // U - Update
+        public Task Delete(T item);             // D - Delete by model
+        public Task DeleteById(int id);         // D - Delete by id
+    }
+}
--- FinalApp.Domain/Models/Enums/WorkTypes.cs
+++ FinalApp.Domain/Models/Enums/WorkTypes.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FinalApp.Domain.Models.Enums
+﻿namespace FinalApp.Domain.Models.Enums
 {
-    internal class WorkTyoes
+    public enum WorkTypes
     {
+        EcoBoxInstallation,
     }
 }
