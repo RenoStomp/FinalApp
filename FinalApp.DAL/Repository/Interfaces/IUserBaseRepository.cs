3a29ea45a9908da9361470e00d6c2a875a73da92
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-20 19:31:48 +0600

    add: summary to user repository added

--- FinalApp.DAL/Repository/Interfaces/IUserBaseRepository.cs
+++ FinalApp.DAL/Repository/Interfaces/IUserBaseRepository.cs
@@ -2,12 +2,38 @@
 
 namespace FinalApp.DAL.Repository.Interfaces
 {
+    /// <summary>
+    /// Represents a repository for managing user base entities.
+    /// </summary>
     public interface IUserBaseRepository
     {
-        public Task<IQueryable<Request>> GetActiveRequests(int techTeamId);
-        public Task<IQueryable<Request>> GetClosedRequests(int techTeamId);
-        public Task AcceptRequest(int requestId, int techTeamId);
-        public Task MarkRequestAsCompleted(int requestId);
+        /// <summary>
+        /// Retrieves the active requests for a technical team.
+        /// </summary>
+        /// <param name="techTeamId">The ID of the technical team.</param>
+        /// <returns>An asynchronous operation that returns the IQueryable of active requests.</returns>
+        Task<IQueryable<Request>> GetActiveRequests(int techTeamId);
 
+        /// <summary>
+        /// Retrieves the closed requests for a technical team.
+        /// </summary>
+        /// <param name="techTeamId">The ID of the technical team.</param>
+        /// <returns>An asynchronous operation that returns the IQueryable of closed requests.</returns>
+        Task<IQueryable<Request>> GetClosedRequests(int techTeamId);
+
+        /// <summary>
+        /// Accepts a request and assigns it to a technical team.
+        /// </summary>
+        /// <param name="requestId">The ID of the request.</param>
+        /// <param name="techTeamId">The ID of the technical team.</param>
+        /// <returns>An asynchronous operation.</returns>
+        Task AcceptRequest(int requestId, int techTeamId);
+
+        /// <summary>
+        /// Marks a request as completed.
+        /// </summary>
+        /// <param name="requestId">The ID of the request.</param>
+        /// <returns>An asynchronous operation.</returns>
+        Task MarkRequestAsCompleted(int requestId);
     }
 }
