bdc478302b8027cd56d4c9bb533e01964ee2d203
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-21 15:31:45 +0600

    add: MapperHelper added

---
+++ FinalApp.Services/Mapping/MapperHelper.cs
@@ -0,0 +1,21 @@
+﻿using FinalApp.ApiModels.DTOs.CommonDTOs.BaseDTOs;
+using FinalApp.Domain.Models.Common;
+
+namespace FinalApp.Services.Mapping
+{
+    public static class MapperHelper<T, Tmodel>
+        where T : BaseEntity
+        where Tmodel : BaseEntityDTO
+    {
+        public static IEnumerable<Tmodel> Map(IEnumerable<T> sourceCollection)
+        {
+            var mapper = AutoMapperConfig<T, Tmodel>.Initialize();
+            return mapper.Map<IEnumerable<Tmodel>>(sourceCollection);
+        }
+        public static Tmodel Map(T source)
+        {
+            var mapper = AutoMapperConfig<T, Tmodel>.Initialize();
+            return mapper.Map<Tmodel>(source);
+        }
+    }
+}
