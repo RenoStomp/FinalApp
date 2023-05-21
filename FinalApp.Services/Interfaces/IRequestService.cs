d8d99883bab704f3f0307823d5210df5b5790f77
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-22 02:12:07 +0600

    add: summary to inerface added

--- FinalApp.Services/Interfaces/IRequestService.cs
+++ FinalApp.Services/Interfaces/IRequestService.cs
@@ -4,15 +4,60 @@ using FinalApp.Domain.Models.Entities.Requests.RequestsInfo;
 
 namespace FinalApp.Services.Interfaces
 {
+    /// <summary>
+    /// Interface for the request service that handles operations related to requests.
+    /// </summary>
     public interface IRequestService
     {
-        public Task<IBaseResponse<IEnumerable<RequestDTO>>> GetUnassignedRequests();
-        public Task<IBaseResponse<IEnumerable<RequestDTO>>> GetClosedRequestsByOperatorId(int operatorId);
-        public Task<IBaseResponse<IEnumerable<RequestDTO>>> GetActiveRequestsByOperatorId(int operatorId);
-        public Task<IBaseResponse<bool>> AssignRequestToTeam(int requestId, int teamId);
-        public Task<IBaseResponse<bool>> AssignRequestToOperator(int requestId, int operatorId);
-        public Task<IBaseResponse<bool>> MarkRequestAsCompleted(int requestId);
-        public Task<IBaseResponse<bool>> AssignLocationToRequest(int requestId, int locationId);
+        /// <summary>
+        /// Retrieves a list of unassigned requests.
+        /// </summary>
+        /// <returns>An asynchronous operation that returns the list of unassigned requests.</returns>
+        Task<IBaseResponse<IEnumerable<RequestDTO>>> GetUnassignedRequests();
 
+        /// <summary>
+        /// Retrieves a list of closed requests assigned to the specified operator.
+        /// </summary>
+        /// <param name="operatorId">The ID of the operator.</param>
+        /// <returns>An asynchronous operation that returns the list of closed requests.</returns>
+        Task<IBaseResponse<IEnumerable<RequestDTO>>> GetClosedRequestsByOperatorId(int operatorId);
+
+        /// <summary>
+        /// Retrieves a list of active requests assigned to the specified operator.
+        /// </summary>
+        /// <param name="operatorId">The ID of the operator.</param>
+        /// <returns>An asynchronous operation that returns the list of active requests.</returns>
+        Task<IBaseResponse<IEnumerable<RequestDTO>>> GetActiveRequestsByOperatorId(int operatorId);
+
+        /// <summary>
+        /// Assigns a request to a team.
+        /// </summary>
+        /// <param name="requestId">The ID of the request to assign.</param>
+        /// <param name="teamId">The ID of the team to assign the request to.</param>
+        /// <returns>An asynchronous operation that returns a response indicating the success or failure of the assignment.</returns>
+        Task<IBaseResponse<bool>> AssignRequestToTeam(int requestId, int teamId);
+
+        /// <summary>
+        /// Assigns a request to an operator.
+        /// </summary>
+        /// <param name="requestId">The ID of the request to assign.</param>
+        /// <param name="operatorId">The ID of the operator to assign the request to.</param>
+        /// <returns>An asynchronous operation that returns a response indicating the success or failure of the assignment.</returns>
+        Task<IBaseResponse<bool>> AssignRequestToOperator(int requestId, int operatorId);
+
+        /// <summary>
+        /// Marks a request as completed.
+        /// </summary>
+        /// <param name="requestId">The ID of the request to mark as completed.</param>
+        /// <returns>An asynchronous operation that returns a response indicating the success or failure of marking the request as completed.</returns>
+        Task<IBaseResponse<bool>> MarkRequestAsCompleted(int requestId);
+
+        /// <summary>
+        /// Assigns a location to a request.
+        /// </summary>
+        /// <param name="requestId">The ID of the request to assign the location to.</param>
+        /// <param name="locationId">The ID of the location to assign to the request.</param>
+        /// <returns>An asynchronous operation that returns a response indicating the success or failure of assigning the location to the request.</returns>
+        Task<IBaseResponse<bool>> AssignLocationToRequest(int requestId, int locationId);
     }
 }
---
+++ FinalApp.Services/Interfaces/IUserBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalApp.Services.Interfaces
+{
+    internal interface IUserBaseService
+    {
+    }
+}
