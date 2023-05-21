77bf006ce731bdfca6a7a26665b56b2e3f60a881
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-21 16:19:07 +0600

    add: Response factory added

---
+++ FinalApp.APIModels/Response/Helpers/ResponseFactory.cs
@@ -0,0 +1,48 @@
+﻿using FinalApp.ApiModels.DTOs.CommonDTOs.BaseDTOs;
+using FinalApp.ApiModels.Response.Implemintations;
+
+namespace FinalApp.ApiModels.Response.Helpers
+{
+    public static class ResponseFactory<TModel>
+        where TModel : BaseEntityDTO
+    {
+        public static BaseResponse<IEnumerable<TModel>> CreateSuccessResponse(IEnumerable<TModel> models)
+        {
+            return new BaseResponse<IEnumerable<TModel>>()
+            {
+                IsSuccess = true,
+                Data = models,
+                StatusCode = 200,
+            };
+        }
+        public static BaseResponse<TModel> CreateSuccessResponse(TModel model)
+        {
+            return new BaseResponse<TModel>
+            {
+                IsSuccess = true,
+                Data = model,
+                StatusCode = 200,
+            };
+        }
+
+        public static BaseResponse<TModel> CreateNotFoundResponse()
+        {
+            return new BaseResponse<TModel>()
+            {
+                Message = "no records found in the database",
+                StatusCode = 0,
+                IsSuccess = false,
+            };
+        }
+        public static BaseResponse<TModel> CreateErrorResponse()
+        {
+            return new BaseResponse<TModel>()
+            {
+                Message = "internal server error",
+                StatusCode = 500,
+                IsSuccess = false,
+            };
+        }
+    }
+
+}
--- FinalApp.Services/Implemintations/ReviewService.cs
+++ FinalApp.Services/Implemintations/ReviewService.cs
@@ -1,4 +1,5 @@
 ﻿using FinalApp.ApiModels.DTOs.EntitiesDTOs.RequestsDTO;
+using FinalApp.ApiModels.Response.Helpers;
 using FinalApp.ApiModels.Response.Implemintations;
 using FinalApp.ApiModels.Response.Interfaces;
 using FinalApp.DAL.Repository.Interfaces;
@@ -30,12 +31,37 @@ namespace FinalApp.Services.Implemintations
                 ObjectValidator<List<Review>>.CheckIsNotNullObject(reviews);
                 IEnumerable<ReviewDTO> reviewsDTO = MapperHelper<Review, ReviewDTO>.Map(reviews);
 
+                return ResponseFactory<ReviewDTO>.CreateSuccessResponse(reviewsDTO);
+            }
+            catch (ArgumentNullException exception)
+            {
+                return ResponseFactory<ReviewDTO>.CreateNotFoundResponse();
+            }
+            catch (Exception ex)
+            {
+                return ResponseFactory<ReviewDTO>.CreateErrorResponse();
+            }
+        }
+
+        public async Task<IBaseResponse<IEnumerable<ReviewDTO>>> GetReviewsByRequestId(int requestId)
+        {
+            try
+            {
+                var reviews = await _repository.ReadAllAsync().Result
+                    .Where(request => request.Request.Id == requestId)
+                    .ToListAsync();
+
+                ObjectValidator<List<Review>>.CheckIsNotNullObject(reviews);
+                IEnumerable<ReviewDTO> reviewsDTO = MapperHelper<Review, ReviewDTO>.Map(reviews);
+
                 return new BaseResponse<IEnumerable<ReviewDTO>>()
                 {
                     IsSuccess = true,
                     Data = reviewsDTO,
                     StatusCode = 200,
                 };
+
+
             }
             catch (ArgumentNullException exception)
             {
@@ -57,17 +83,22 @@ namespace FinalApp.Services.Implemintations
             }
         }
 
-        public async Task<IBaseResponse<IEnumerable<ReviewDTO>>> GetReviewsByRequestId(int requestId)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<IBaseResponse<bool>> CanCreateReview(int requestId)
         {
             throw new NotImplementedException();
         }
 
         public async Task<IBaseResponse<ReviewDTO>> CreateReview(int requestId, string reviewText, int evaluation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<bool>> CanUpdateReview(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IBaseResponse<ReviewDTO>> UpdateReview(int reviewId, string reviewText, int evaluation)
         {
             throw new NotImplementedException();
         }
