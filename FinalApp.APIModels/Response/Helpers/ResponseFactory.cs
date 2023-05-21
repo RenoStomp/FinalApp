73d4ebdd9c174ee8f7ec01259762323a1215aff9
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-21 17:31:40 +0600

    add:  Review service added

--- FinalApp.APIModels/Response/Helpers/ResponseFactory.cs
+++ FinalApp.APIModels/Response/Helpers/ResponseFactory.cs
@@ -3,70 +3,78 @@ using FinalApp.ApiModels.Response.Implemintations;
 
 namespace FinalApp.ApiModels.Response.Helpers
 {
-    public static class ResponseFactory<TModel>
-        where TModel : BaseEntityDTO
+    public static class ResponseFactory<T>
     {
-        public static BaseResponse<IEnumerable<TModel>> CreateSuccessResponseForModelCollection(IEnumerable<TModel> models)
+        public static BaseResponse<IEnumerable<T>> CreateSuccessResponseForModelCollection(IEnumerable<T> models)
         {
-            return new BaseResponse<IEnumerable<TModel>>()
+            return new BaseResponse<IEnumerable<T>>()
             {
                 IsSuccess = true,
                 Data = models,
                 StatusCode = 200,
             };
         }
-        public static BaseResponse<TModel> CreateSuccessResponseForOneModel(TModel model)
+        public static BaseResponse<T> CreateSuccessResponseForOneModel(T model)
         {
-            return new BaseResponse<TModel>
+            return new BaseResponse<T>
             {
                 IsSuccess = true,
                 Data = model,
                 StatusCode = 200,
             };
         }
-        public static BaseResponse<IEnumerable<TModel>> CreateNotFoundResponseForModelCollection(Exception exception)
+        public static BaseResponse<IEnumerable<T>> CreateNotFoundResponseForModelCollection(Exception exception)
         {
-            return new BaseResponse<IEnumerable<TModel>>()
+            return new BaseResponse<IEnumerable<T>>()
             {        
                 StatusCode = 0,
                 IsSuccess = false,
-                Message = "no records found in the database\n\r" +
+                Message = "no records found in the database.\n\r" +
                     $"Error: {exception}",
             };
         }
 
-        public static BaseResponse<TModel> CreateNotFoundResponseForOneModel(Exception exception)
+        public static BaseResponse<T> CreateNotFoundResponseForOneModel(Exception exception)
         {
-            return new BaseResponse<TModel>()
+            return new BaseResponse<T>()
             {
                 StatusCode = 0,
                 IsSuccess = false,
-                Message = "no records found in the database\n\r" +
+                Message = "no records found in the database.\n\r" +
                     $"Error: {exception}",
             };
         }
 
-        public static BaseResponse<IEnumerable<TModel>> CreateErrorResponseForModelCollection(Exception exception)
+        public static BaseResponse<IEnumerable<T>> CreateErrorResponseForModelCollection(Exception exception)
         {
-            return new BaseResponse<IEnumerable<TModel>>()
+            return new BaseResponse<IEnumerable<T>>()
             {             
                 StatusCode = 500,
                 IsSuccess = false,
-                Message = "internal server error\n\r" +
+                Message = "internal server error.\n\r" +
                 $"Error: {exception}",
             };
         }
 
-        public static BaseResponse<TModel> CreateErrorResponseForOneModel(Exception exception)
+        public static BaseResponse<T> CreateErrorResponseForOneModel(Exception exception)
         {
-            return new BaseResponse<TModel>()
+            return new BaseResponse<T>()
             {
                 StatusCode = 500,
                 IsSuccess = false,
-                Message = "internal server error\n\r" +
+                Message = "internal server error.\n\r" +
+                $"Error: {exception}",
+            };
+        }
+        public static BaseResponse<T> CreateInvalidOperationResponseForOneModel(Exception exception)
+        {
+            return new BaseResponse<T>()
+            {
+                StatusCode = 400,
+                IsSuccess = false,
+                Message = "Unable to create the specified model.\n\r" +
                 $"Error: {exception}",
             };
         }
     }
-
 }
--- FinalApp.Services/Implemintations/ReviewService.cs
+++ FinalApp.Services/Implemintations/ReviewService.cs
@@ -1,9 +1,9 @@
 ﻿using FinalApp.ApiModels.DTOs.EntitiesDTOs.RequestsDTO;
 using FinalApp.ApiModels.Response.Helpers;
-using FinalApp.ApiModels.Response.Implemintations;
 using FinalApp.ApiModels.Response.Interfaces;
 using FinalApp.DAL.Repository.Interfaces;
 using FinalApp.Domain.Models.Entities.Requests.RequestsInfo;
+using FinalApp.Domain.Models.Enums;
 using FinalApp.Services.Interfaces;
 using FinalApp.Services.Mapping;
 using FinallApp.ValidationHelper;
@@ -13,11 +13,15 @@ namespace FinalApp.Services.Implemintations
 {
     internal class ReviewService : IReviewService
     {
+        private const int MinEvaluation = 1;
+        private const int MaxEvaluation = 10;
         public readonly IBaseAsyncRepository<Review> _repository;
+        public readonly IBaseAsyncRepository<Request> _requestRepository;
 
-        public ReviewService(IBaseAsyncRepository<Review> repository)
+        public ReviewService(IBaseAsyncRepository<Review> repository, IBaseAsyncRepository<Request> requestRepository)
         {
             _repository = repository;
+            _requestRepository = requestRepository;
         }
 
         public async Task<IBaseResponse<IEnumerable<ReviewDTO>>> GetReviewsByEvaluation(int evaluation)
@@ -33,13 +37,13 @@ namespace FinalApp.Services.Implemintations
 
                 return ResponseFactory<ReviewDTO>.CreateSuccessResponseForModelCollection(reviewsDTO);
             }
-            catch (ArgumentNullException exception)
+            catch (ArgumentNullException argNullException)
             {
-                return ResponseFactory<ReviewDTO>.CreateNotFoundResponseForModelCollection();
+                return ResponseFactory<ReviewDTO>.CreateNotFoundResponseForModelCollection(argNullException);
             }
-            catch (Exception exp)
+            catch (Exception excpetion)
             {
-                return ResponseFactory<ReviewDTO>.CreateErrorResponseForModelCollection();
+                return ResponseFactory<ReviewDTO>.CreateErrorResponseForModelCollection(excpetion);
             }
         }
 
@@ -56,34 +60,142 @@ namespace FinalApp.Services.Implemintations
 
                 return ResponseFactory<ReviewDTO>.CreateSuccessResponseForModelCollection(reviewsDTO);
             }
-            catch (ArgumentNullException)
+            catch (ArgumentNullException argNullException)
             {
-                return ResponseFactory<ReviewDTO>.CreateNotFoundResponseForModelCollection();
+                return ResponseFactory<ReviewDTO>.CreateNotFoundResponseForModelCollection(argNullException);
             }
-            catch (Exception)
+            catch (Exception exception)
             {
-                return ResponseFactory<ReviewDTO>.CreateErrorResponseForModelCollection();
+                return ResponseFactory<ReviewDTO>.CreateErrorResponseForModelCollection(exception);
             }
         }
 
         public async Task<IBaseResponse<bool>> CanCreateReview(int requestId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var request = await _requestRepository.ReadAllAsync().Result
+                    .FirstOrDefaultAsync(r => r.Id == requestId);
+
+                if (request == null || request.RequestStatus != Status.Completed || request.Review != null)
+                    throw new InvalidOperationException("Unable to create a review for the specified request.");
+
+                ObjectValidator<Request>.CheckIsNotNullObject(request);
+
+                var canCreateReview = request.RequestStatus == Status.Completed && request.Review == null;
+                return ResponseFactory<bool>.CreateSuccessResponseForOneModel(canCreateReview);
+            }
+            catch (InvalidOperationException invException)
+            {
+                return ResponseFactory<bool>.CreateInvalidOperationResponseForOneModel(invException);
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                return ResponseFactory<bool>.CreateNotFoundResponseForOneModel(argNullException);
+            }
+            catch (Exception exception)
+            {
+                return ResponseFactory<bool>.CreateErrorResponseForOneModel(exception);
+            }
         }
 
         public async Task<IBaseResponse<ReviewDTO>> CreateReview(int requestId, string reviewText, int evaluation)
         {
-            throw new NotImplementedException();
+            try
+            {
+                StringValidator.CheckIsNotNull(reviewText);
+                NumberValidator<int>.IsRange(evaluation, MinEvaluation, MaxEvaluation);
+
+                Request request = await _requestRepository.ReadAllAsync().Result
+                    .FirstOrDefaultAsync(r => r.Id == requestId);
+
+
+                if (request == null || request.RequestStatus != Status.Completed || request.Review != null)
+                    throw new InvalidOperationException("Unable to create review for the specified request.");
+
+
+                var review = new Review
+                {
+                    ReviewText = reviewText,
+                    Evaluation = evaluation
+                };
+
+                await _repository.Create(review);
+                request.Review = review;
+
+
+                return ResponseFactory<ReviewDTO>.CreateSuccessResponseForOneModel(MapperHelper<Review, ReviewDTO>.Map(review));
+            }
+            catch (InvalidOperationException invException)
+            {
+                return ResponseFactory<ReviewDTO>.CreateInvalidOperationResponseForOneModel(invException);
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                return ResponseFactory<ReviewDTO>.CreateNotFoundResponseForOneModel(argNullException);
+            }
+            catch (Exception exception)
+            {
+                return ResponseFactory<ReviewDTO>.CreateErrorResponseForOneModel(exception);
+            }
         }
 
-        public Task<IBaseResponse<bool>> CanUpdateReview(int reviewId)
+        public async Task<IBaseResponse<bool>> CanUpdateReview(int reviewId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var review = await _repository.ReadByIdAsync(reviewId);
+
+                ObjectValidator<Review>.CheckIsNotNullObject(review);
+
+                var canUpdateReview = review != null && review.Request.RequestStatus == Status.Completed;
+
+                return ResponseFactory<bool>.CreateSuccessResponseForOneModel(canUpdateReview);
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                return ResponseFactory<bool>.CreateNotFoundResponseForOneModel(argNullException);
+            }
+            catch (Exception exception)
+            {
+                return ResponseFactory<bool>.CreateErrorResponseForOneModel(exception);
+            }
         }
 
-        public Task<IBaseResponse<ReviewDTO>> UpdateReview(int reviewId, string reviewText, int evaluation)
+        public async Task<IBaseResponse<ReviewDTO>> UpdateReview(int reviewId, string reviewText, int evaluation)
         {
-            throw new NotImplementedException();
+            try
+            {
+                StringValidator.CheckIsNotNull(reviewText);
+                NumberValidator<int>.IsRange(evaluation, MinEvaluation, MaxEvaluation);
+
+                var review = await _repository.ReadByIdAsync(reviewId);
+
+                if (review == null || review.Request.RequestStatus != Status.Completed)
+                    throw new InvalidOperationException("Unable to update the specified review.");
+
+                review.ReviewText = reviewText;
+                review.Evaluation = evaluation;
+
+                await _repository.UpdateAsync(review);
+
+                ObjectValidator<Review>.CheckIsNotNullObject(review);
+                ReviewDTO reviewsDTO = MapperHelper<Review, ReviewDTO>.Map(review);
+
+                return ResponseFactory<ReviewDTO>.CreateSuccessResponseForOneModel(reviewsDTO);
+            }
+            catch (InvalidOperationException invException)
+            {
+                return ResponseFactory<ReviewDTO>.CreateInvalidOperationResponseForOneModel(invException);
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                return ResponseFactory<ReviewDTO>.CreateNotFoundResponseForOneModel(argNullException);
+            }
+            catch (Exception exception)
+            {
+                return ResponseFactory<ReviewDTO>.CreateErrorResponseForOneModel(exception);
+            }
         }
     }
 }
