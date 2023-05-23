3ee42c4cd4381955931cfbef9ce7e4a060121981
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-05-23 13:31:14 +0600

    add: summary added

--- FinalApp.Services/Mapping/EntityAutoMapperConfig.cs
+++ FinalApp.Services/Mapping/EntityAutoMapperConfig.cs
@@ -2,9 +2,17 @@
 
 namespace FinalApp.Services.Mapping
 {
+    /// <summary>
+    /// Configuration class for AutoMapper to map entities between two models.
+    /// </summary>
+    /// <typeparam name="Tmodel">Source model type.</typeparam>
+    /// <typeparam name="T">Destination model type.</typeparam>
     public class EntityAutoMapperConfig<Tmodel, T>
     {
-
+        /// <summary>
+        /// Initializes and configures AutoMapper for mapping between source and destination models.
+        /// </summary>
+        /// <returns>An instance of IMapper for performing the mappings.</returns>
         public static IMapper Initialize()
         {
             var mapperConfiguration = new MapperConfiguration(cfg =>
--- FinalApp.Services/Mapping/MapperHelperForEntity.cs
+++ FinalApp.Services/Mapping/MapperHelperForEntity.cs
@@ -4,10 +4,20 @@ using FinallApp.ValidationHelper;
 
 namespace FinalApp.Services.Mapping
 {
+    /// <summary>
+    /// Helper class for mapping between model objects and entity objects using AutoMapper.
+    /// </summary>
+    /// <typeparam name="Tmodel">The model type, derived from BaseEntityDTO.</typeparam>
+    /// <typeparam name="T">The entity type, derived from BaseEntity.</typeparam>
     public class MapperHelperForEntity<Tmodel, T>
         where Tmodel : BaseEntityDTO
         where T : BaseEntity
     {
+        /// <summary>
+        /// Maps a collection of model objects to a collection of entity objects.
+        /// </summary>
+        /// <param name="sourceCollection">The source collection of model objects.</param>
+        /// <returns>The mapped collection of entity objects.</returns>
         public static IEnumerable<T> Map(IEnumerable<Tmodel> sourceCollection)
         {
             ObjectValidator<IEnumerable<Tmodel>>.CheckIsNotNullObject(sourceCollection);
@@ -15,6 +25,12 @@ namespace FinalApp.Services.Mapping
             var mapper = EntityAutoMapperConfig<Tmodel, T>.Initialize();
             return mapper.Map<IEnumerable<T>>(sourceCollection);
         }
+
+        /// <summary>
+        /// Maps a model object to an entity object.
+        /// </summary>
+        /// <param name="source">The source model object.</param>
+        /// <returns>The mapped entity object.</returns>
         public static T Map(Tmodel source)
         {
             ObjectValidator<Tmodel>.CheckIsNotNullObject(source);
